73f68c7ec6bdb7be6c93276c3638d711e3de2392
AgusBalles <ag.ballesteros@duocuc.cl>
2025-10-17 18:27:54 -0300

    casi funciona

---
+++ src/pages/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';  
+export  default function Index() {
+return (
+  <>
+  <h1>Welcome to the Home Page</h1>
+  <p>This is the home page of our application.</p>
+  </>
+  )
+}
